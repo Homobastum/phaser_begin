0c035864bee217ca5391c5c59e97fc6a50d2e315
Mathieu GONTHIER <gonthier.mhc@gmail.com>
2018-10-24 23:26:55 +0400

    Automatically start the state Boot

--- ts/game.ts
+++ ts/game.ts
@@ -16,15 +16,12 @@ module Begin {
              * Gestion des écrans (ou states) du jeu *
              *****************************************/
             // Ajout des différents écrans
-            this.state.add('Boot', Boot, false);
+            this.state.add('Boot', Boot, true);
             this.state.add('Preloader', Preloader, false);
             this.state.add('TitleScreen', TitleScreen, false);
             this.state.add('TitleMenu', TitleMenu, false);
             this.state.add('Map1', Map1, false);
             this.state.add('Map2', Map2, false);
-
-            // Lancement du premier écran
-            this.state.start('Boot');
         }
     }
 }
